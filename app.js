d554b28569ffe26712b4d346e3aa79486db5db6a
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-02-19 23:47:39 -0600

    example for inquirer

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
-// purpose of app.js file is to execute functions to capture user input and create a file
+// // purpose of app.js file is to execute functions to capture user input and create a file
 
-// tells node to use the File System module
-const fs = require('fs');
+// tell node to use the inquirer module that was downloaded
+const inquirer = require('inquirer');
+ 
+// // tells node to use the File System module
+// const fs = require('fs');
 
-// for destination file that we want to receive the exported functions from src file from
-// receiving generatePage() function from page-template.js file --> need relative path
-// the object in the module.exports assignment will be reassigned to the generatePage variable in the app.js file
-// the relative path to include the file must be exact.
-const generatePage = require('./src/page-template')
+// // for destination file that we want to receive the exported functions from src file from
+// // receiving generatePage() function from page-template.js file --> need relative path
+// // the object in the module.exports assignment will be reassigned to the generatePage variable in the app.js file
+// // the relative path to include the file must be exact.
+// const generatePage = require('./src/page-template')
 
-// holds the users command line arguments, starting at index 2 (not 0 nor 1) and on
-const profileDataArgs = process.argv.slice(2);
+// // get the code from page-template.js
+// const pageHTML = generatePage(name1, github);
 
-// capture user input
-// save the indexes from the array formed by the command line arguments
-const [name, github] = profileDataArgs;
+// // create a file
+// // writes the code from the previous function into the html file
+// fs.writeFile('./index.html', pageHTML, err => {
+//    // handles errors
+//   if (err) throw new Error(err);
 
-// create a file
-// writes the code from the previous function into the html file
-fs.writeFile('./index.html', generatePage(name, github), err => {
-   // handles errors
-  if (err) throw new Error(err);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
--- module-exs.js
+++ module-exs.js
@@ -1,3 +1,33 @@
+// START OF 9.3.5
+// purpose of app.js file is to execute functions to capture user input and create a file
+
+// tells node to use the File System module
+const fs = require('fs');
+
+// for destination file that we want to receive the exported functions from src file from
+// receiving generatePage() function from page-template.js file --> need relative path
+// the object in the module.exports assignment will be reassigned to the generatePage variable in the app.js file
+// the relative path to include the file must be exact.
+const generatePage = require('./src/page-template')
+
+// holds the users command line arguments, starting at index 2 (not 0 nor 1) and on
+const profileDataArgs = process.argv.slice(2);
+
+// capture user input
+// save the indexes from the array formed by the command line arguments
+const [name, github] = profileDataArgs;
+
+// create a file
+// writes the code from the previous function into the html file
+fs.writeFile('./index.html', generatePage(name, github), err => {
+   // handles errors
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+/////////////////////////////////////////////////////////////////////////////
+
 // this is cutting out the first two indexes of the array, from the command line, which are --> node app.js 
 // and saving the rest of the indexes from the command line
 // leaves out indexes 0, 1 and the number that = process.argv.length - gets everything in between
